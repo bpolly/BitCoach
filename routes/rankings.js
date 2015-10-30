1237cb5a2565acc29728b20e9b16cd71d2bf54ba
Braden Polly <bpolly6@gmail.com>
2015-10-29 22:19:24 -0400

    Rankings index page now generates thumbnails for each site. This layout will probably switch layouts with the current Site index soon.

--- routes/rankings.js
+++ routes/rankings.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Bit Coach' });
-  res.render('rankings', { title: 'Rankings' });
+  var Site = require("../models/site").Site
+  Site.find({}).exec(function(err, results) {
+        console.log(results);
+        console.log(JSON.stringify(results));
+        res.render('rankings', { title: 'Rankings', sites: results });
+      });
+
 });
 
 module.exports = router;
